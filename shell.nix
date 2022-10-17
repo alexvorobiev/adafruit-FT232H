3fac0cf6d1a77542f608e825bef60682faaa309b
Alex Vorobiev <alexander.vorobiev@gmail.com>
2022-10-17 17:04:03 -0500

    Working configuration

--- shell.nix
+++ shell.nix
@@ -1,13 +1,17 @@
 { pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
 with pkgs;
+let
+  adafruit-circuitpython-typing = python3.pkgs.callPackage ./adafruit-circuitpython-typing {};
+in
 mkShell {
+  BLINKA_FT232H=1;
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = [
     (python3.withPackages (ps: with python3Packages; [
       ipython
       python-lsp-server
       pyftdi
-      (python3.pkgs.callPackage ./adafruit-blinka {})
+      (python3.pkgs.callPackage ./adafruit-blinka {  })
     ]))
     libusb1
     # usbip-linux
